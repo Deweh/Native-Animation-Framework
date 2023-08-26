29bdd4808e31844c01dc41c8a9e2deb00b424e14
Niston Cloud <niston.cloud@outlook.com>
2023-08-26 06:33:22 +0200

    Added Papyrus bindings for Set/GetRescalerDisabled

--- f4se-plugin/src/Scripts/Papyrus.h
+++ f4se-plugin/src/Scripts/Papyrus.h
@@ -11,6 +11,8 @@ namespace Papyrus
 	bool RegisterFunctions(RE::BSScript::IVirtualMachine* a_VM)
 	{
 		PAPYRUS_BIND(ToggleMenu);
+		PAPYRUS_BIND(SetRescalerDisabled);
+		PAPYRUS_BIND(GetRescalerDisabled);		
 		PAPYRUS_BIND(IsActorUsable);
 		PAPYRUS_BIND(SetActorUsable);
 		PAPYRUS_BIND(SetPackageOverride);
