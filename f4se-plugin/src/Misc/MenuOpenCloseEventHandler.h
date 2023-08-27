755f7b3fe2e895b29db2cc29e225914c783abdad
Niston Cloud <niston.cloud@outlook.com>
2023-08-28 00:04:32 +0200

    removed some debug traces

--- f4se-plugin/src/Misc/MenuOpenCloseEventHandler.h
+++ f4se-plugin/src/Misc/MenuOpenCloseEventHandler.h
@@ -15,21 +15,13 @@ public:
 
 	RE::BSEventNotifyControl ProcessEvent(const RE::MenuOpenCloseEvent& event, MenuOpenCloseEventSource*)
 	{
-		//logger::trace("MenuOpenCloseEvent ({}), opening is ({})", event.menuName.c_str(), event.opening);
-		//your code here on menu open/close
 		if (event.menuName == "ContainerMenu" && event.opening)
 		{
-			// opening menu
-			logger::trace("ContainerMenu opened");
 			auto ui = RE::UI::GetSingleton();
 			ui->UpdateMenuMode("ContainerMenu", false);
 		}
 		else if(event.menuName == "ContainerMenu" && !event.opening)
-		{
-			// closing
-			logger::trace("Containermenu closed");
-
-			
+		{		
 			if (auto sceneData = Menu::PersistentMenuState::SceneData::GetSingleton(); sceneData->restoreSubmenu != Menu::SUB_MENU_TYPE::kNone)
 			{
 				F4SE::GetTaskInterface()->AddTask([]() {
